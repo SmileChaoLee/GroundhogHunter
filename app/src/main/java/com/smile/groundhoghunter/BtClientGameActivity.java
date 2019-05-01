9fd00c85f80ed448894ef07f0dc8e0cf8e000df2
chaolee <leechao1959@gmail.com>
2019-05-01 12:21:03 -0400

    1. Created an activity ClientGameActivity 2. BtClientGameActivity (for Bluetooth client game) derived from ClientGameActivity

--- app/src/main/java/com/smile/groundhoghunter/BtClientGameActivity.java
+++ app/src/main/java/com/smile/groundhoghunter/BtClientGameActivity.java
@@ -2,19 +2,11 @@ package com.smile.groundhoghunter;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.Looper;
-import android.os.Message;
-import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
-
-import com.smile.groundhoghunter.Constants.CommonConstants;
 import com.smile.groundhoghunter.Threads.BluetoothFunctionThread;
 import com.smile.groundhoghunter.Utilities.BluetoothUtil;
-import com.smile.smilepublicclasseslibrary.utilities.ScreenUtil;
 
-public class BtClientGameActivity extends GroundhogActivity {
+public class BtClientGameActivity extends ClientGameActivity {
 
     private final static String TAG = "BtClientGameActivity";
     private BluetoothFunctionThread selectedBtFunctionThread;
@@ -23,18 +15,9 @@ public class BtClientGameActivity extends GroundhogActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         BtClientGameHandler btClientGameHandler = new BtClientGameHandler(Looper.getMainLooper(), this);
-        selectedBtFunctionThread = (BluetoothFunctionThread) GroundhogHunterApp.selectedIoFuncThread;
+        selectedBtFunctionThread = (BluetoothFunctionThread) super.selectedIoFunctionThread;
         selectedBtFunctionThread.setHandler(btClientGameHandler);
         selectedBtFunctionThread.setStartRead(true);    // start reading data
-
-        // setContentView(R.layout.activity_client_bt_game);
-
-        startGameButton.setVisibility(View.INVISIBLE);
-        startGameButton.setEnabled(false);
-        // pauseGameButton.setEnabled(false);
-        // resumeGameButton.setEnabled(false);
-        newGameButton.setVisibility(View.INVISIBLE);
-        newGameButton.setEnabled(false);
     }
 
     @Override
@@ -44,106 +27,11 @@ public class BtClientGameActivity extends GroundhogActivity {
         super.onDestroy();
     }
 
-    @Override
-    protected void pauseGame() {
-        selectedBtFunctionThread.write(CommonConstants.TwoPlayerPauseGameButton, "");
-        gameView.pauseGame();
-        pauseGameButton.setEnabled(false);
-        pauseGameButton.setVisibility(View.INVISIBLE);
-        resumeGameButton.setEnabled(true);
-        resumeGameButton.setVisibility(View.VISIBLE);
-    }
-
-    @Override
-    protected void resumeGame() {
-        selectedBtFunctionThread.write(CommonConstants.TwoPlayerResumeGameButton, "");
-        gameView.resumeGame();
-        pauseGameButton.setEnabled(true);
-        pauseGameButton.setVisibility(View.VISIBLE);
-        resumeGameButton.setEnabled(false);
-        resumeGameButton.setVisibility(View.INVISIBLE);
-    }
-
-    @Override
-    protected void quitGame() {
-        selectedBtFunctionThread.write(CommonConstants.TwoPlayerOppositeLeftGame, "");
-        super.quitGame();
-    }
-
-    private class BtClientGameHandler extends Handler {
-
-        private final Looper mLooper;
-        private final Context mContext;
+    private class BtClientGameHandler extends ClientGameHandler {
 
         public BtClientGameHandler(Looper looper, Context context) {
-            super(looper);
-            mLooper = looper;
-            mContext = context;
-        }
+            super(looper, context);
 
-        @Override
-        public void handleMessage(Message msg) {
-            // super.handleMessage(msg);
-
-            String msgString = "";
-
-            Context mContext = getApplicationContext();
-            Bundle data = msg.getData();
-
-            Log.d(TAG, "Message received: " + msg.what);
-            switch (msg.what) {
-                case CommonConstants.TwoPlayerOppositeLeftGame:
-                    // received by host and client sides
-                    msgString = mContext.getString(R.string.oppositePlayerLeftGameString);
-                    ScreenUtil.showToast(mContext, msgString, toastTextSize, GroundhogHunterApp.FontSize_Scale_Type, Toast.LENGTH_SHORT);
-                    break;
-                case CommonConstants.TwoPlayerNewGameButton:
-                    // received by client side
-                    // ScreenUtil.showToast(mContext, "Host pressed new game button.", toastTextSize, GroundhogHunterApp.FontSize_Scale_Type, Toast.LENGTH_SHORT);
-                    gameView.newGame(); // new game on client side
-                    pauseGameButton.setEnabled(false);
-                    pauseGameButton.setVisibility(View.INVISIBLE);
-                    resumeGameButton.setEnabled(false);
-                    resumeGameButton.setVisibility(View.INVISIBLE);
-                    break;
-                case CommonConstants.TwoPlayerStartGameButton:
-                    // received by client side
-                    // ScreenUtil.showToast(mContext, "Host pressed start game button.", toastTextSize, GroundhogHunterApp.FontSize_Scale_Type, Toast.LENGTH_SHORT);
-                    gameView.startGame();   // start game on client side
-                    pauseGameButton.setEnabled(true);
-                    pauseGameButton.setVisibility(View.VISIBLE);
-                    resumeGameButton.setEnabled(false);
-                    resumeGameButton.setVisibility(View.INVISIBLE);
-                    break;
-                case CommonConstants.TwoPlayerPauseGameButton:
-                    // received by host and client sides
-                    // ScreenUtil.showToast(mContext, "Opposite player pressed pause game button.", toastTextSize, GroundhogHunterApp.FontSize_Scale_Type, Toast.LENGTH_SHORT);
-                    gameView.pauseGame();
-                    pauseGameButton.setEnabled(false);
-                    pauseGameButton.setVisibility(View.INVISIBLE);
-                    resumeGameButton.setEnabled(true);
-                    resumeGameButton.setVisibility(View.VISIBLE);
-                    break;
-                case CommonConstants.TwoPlayerResumeGameButton:
-                    // received by host and client sides
-                    // ScreenUtil.showToast(mContext, "Opposite player pressed resume game button.", toastTextSize, GroundhogHunterApp.FontSize_Scale_Type, Toast.LENGTH_SHORT);
-                    gameView.resumeGame();
-                    resumeGameButton.setEnabled(false);
-                    resumeGameButton.setVisibility(View.INVISIBLE);
-                    pauseGameButton.setEnabled(true);
-                    pauseGameButton.setVisibility(View.VISIBLE);
-                    break;
-                case CommonConstants.TwoPlayerDefaultReading:
-                default:
-                    // wrong or read error
-                    break;
-
-            }
-
-            if (selectedBtFunctionThread != null) {
-                // read the next data
-                selectedBtFunctionThread.setStartRead(true);    // start reading data
-            }
         }
     }
 }
---
+++ app/src/main/java/com/smile/groundhoghunter/ClientGameActivity.java
@@ -0,0 +1,137 @@
+package com.smile.groundhoghunter;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.smile.groundhoghunter.AbstractClasses.IoFunctionThread;
+import com.smile.groundhoghunter.Constants.CommonConstants;
+import com.smile.smilepublicclasseslibrary.utilities.ScreenUtil;
+
+public class ClientGameActivity extends GroundhogActivity {
+
+    private final static String TAG = "ClientGameActivity";
+    protected IoFunctionThread selectedIoFunctionThread;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startGameButton.setVisibility(View.INVISIBLE);
+        startGameButton.setEnabled(false);
+        // pauseGameButton.setEnabled(false);
+        // resumeGameButton.setEnabled(false);
+        newGameButton.setVisibility(View.INVISIBLE);
+        newGameButton.setEnabled(false);
+
+        selectedIoFunctionThread = GroundhogHunterApp.selectedIoFuncThread;
+    }
+
+    @Override
+    protected void pauseGame() {
+        selectedIoFunctionThread.write(CommonConstants.TwoPlayerPauseGameButton, "");
+        gameView.pauseGame();
+        pauseGameButton.setEnabled(false);
+        pauseGameButton.setVisibility(View.INVISIBLE);
+        resumeGameButton.setEnabled(true);
+        resumeGameButton.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    protected void resumeGame() {
+        selectedIoFunctionThread.write(CommonConstants.TwoPlayerResumeGameButton, "");
+        gameView.resumeGame();
+        pauseGameButton.setEnabled(true);
+        pauseGameButton.setVisibility(View.VISIBLE);
+        resumeGameButton.setEnabled(false);
+        resumeGameButton.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    protected void quitGame() {
+        selectedIoFunctionThread.write(CommonConstants.TwoPlayerOppositeLeftGame, "");
+        super.quitGame();
+    }
+
+    protected class ClientGameHandler extends Handler {
+
+        private final Looper mLooper;
+        private final Context mContext;
+
+        public ClientGameHandler(Looper looper, Context context) {
+            super(looper);
+            mLooper = looper;
+            mContext = context;
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            // super.handleMessage(msg);
+
+            String msgString = "";
+
+            Context mContext = getApplicationContext();
+            Bundle data = msg.getData();
+
+            Log.d(TAG, "Message received: " + msg.what);
+            switch (msg.what) {
+                case CommonConstants.TwoPlayerOppositeLeftGame:
+                    // received by host and client sides
+                    msgString = mContext.getString(R.string.oppositePlayerLeftGameString);
+                    ScreenUtil.showToast(mContext, msgString, toastTextSize, GroundhogHunterApp.FontSize_Scale_Type, Toast.LENGTH_SHORT);
+                    break;
+                case CommonConstants.TwoPlayerNewGameButton:
+                    // received by client side
+                    // ScreenUtil.showToast(mContext, "Host pressed new game button.", toastTextSize, GroundhogHunterApp.FontSize_Scale_Type, Toast.LENGTH_SHORT);
+                    gameView.newGame(); // new game on client side
+                    pauseGameButton.setEnabled(false);
+                    pauseGameButton.setVisibility(View.INVISIBLE);
+                    resumeGameButton.setEnabled(false);
+                    resumeGameButton.setVisibility(View.INVISIBLE);
+                    break;
+                case CommonConstants.TwoPlayerStartGameButton:
+                    // received by client side
+                    // ScreenUtil.showToast(mContext, "Host pressed start game button.", toastTextSize, GroundhogHunterApp.FontSize_Scale_Type, Toast.LENGTH_SHORT);
+                    gameView.startGame();   // start game on client side
+                    pauseGameButton.setEnabled(true);
+                    pauseGameButton.setVisibility(View.VISIBLE);
+                    resumeGameButton.setEnabled(false);
+                    resumeGameButton.setVisibility(View.INVISIBLE);
+                    break;
+                case CommonConstants.TwoPlayerPauseGameButton:
+                    // received by host and client sides
+                    // ScreenUtil.showToast(mContext, "Opposite player pressed pause game button.", toastTextSize, GroundhogHunterApp.FontSize_Scale_Type, Toast.LENGTH_SHORT);
+                    gameView.pauseGame();
+                    pauseGameButton.setEnabled(false);
+                    pauseGameButton.setVisibility(View.INVISIBLE);
+                    resumeGameButton.setEnabled(true);
+                    resumeGameButton.setVisibility(View.VISIBLE);
+                    break;
+                case CommonConstants.TwoPlayerResumeGameButton:
+                    // received by host and client sides
+                    // ScreenUtil.showToast(mContext, "Opposite player pressed resume game button.", toastTextSize, GroundhogHunterApp.FontSize_Scale_Type, Toast.LENGTH_SHORT);
+                    gameView.resumeGame();
+                    resumeGameButton.setEnabled(false);
+                    resumeGameButton.setVisibility(View.INVISIBLE);
+                    pauseGameButton.setEnabled(true);
+                    pauseGameButton.setVisibility(View.VISIBLE);
+                    break;
+                case CommonConstants.TwoPlayerDefaultReading:
+                default:
+                    // wrong or read error
+                    break;
+
+            }
+
+            if (selectedIoFunctionThread != null) {
+                // read the next data
+                selectedIoFunctionThread.setStartRead(true);    // start reading data
+            }
+        }
+    }
+}
